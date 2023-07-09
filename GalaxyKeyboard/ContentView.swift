ed00b30c65199ae6c296c8d0caa5a02337780b1f
Łukasz Kudzia <qdzia.work@gmail.com>
2023-07-09 16:49:19 +0200

    Init a project

---
+++ GalaxyKeyboard/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GalaxyKeyboard
+//
+//  Created by Łukasz Kudzia on 09/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ GalaxyKeyboard/GalaxyKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyKeyboardApp.swift
+//  GalaxyKeyboard
+//
+//  Created by Łukasz Kudzia on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
